1be83f522b52b5170158e34c8baa32f5f71dbca9
Rayza Oliveira <rayza.ocr@gmail.com>
2021-05-14 19:43:17 -0300

    feat: importing scraps in the index file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-// O arquivo /index.js etá sendo usado como modelo para o arquivo /queroBolsa.js
-
-// Importe a biblioteca do dramaturgo para o nosso raspador.
-const playwright = require("playwright");
+const scrapingBuscaPe = require("./buscaPe");
+const scrapingQueroBolsa = require("./queroBolsa");
 
 async function main() {
-  // Abra um navegador Chromium. Usamos headless: false
-  // para poder observar o que está acontecendo.
-  const browser = await playwright.chromium.launch({
-    headless: false,
-  });
-  // Abra uma nova página / guia no navegador.
-  const page = await browser.newPage();
-  // Diga à guia para navegar até a página do tópico JavaScript.
-  await page.goto("https://github.com/topics/javascript");
-  // Faça uma pausa de 10 segundos para ver o que está acontecendo.
-  await page.waitForTimeout(10000);
-  // Desligue o navegador para limpar depois de nós mesmos.
-  await browser.close();
+  console.log(
+    "---------------------STARTING SCRAP OF THE BUSCAPE------------------------"
+  );
+
+  await scrapingBuscaPe();
+
+  console.log(
+    "---------------------FINISH SCRAP OF THE BUSCAPE--------------------------"
+  );
+  console.log("\n\n");
+  console.log(
+    "---------------------STARTING SCRAP OF THE QUERO BOLSA--------------------"
+  );
+
+  await scrapingQueroBolsa();
+
+  console.log(
+    "---------------------FINISH SCRAP OF THE QUERO BOLSA----------------------"
+  );
 }
 
 main();
-
-// Observações:
-// link: https://blog.apify.com/how-to-scrape-the-web-with-playwright-ece1ced75f73
